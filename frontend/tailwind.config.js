878dcdb0c2997f4deecdee283eceeb95c53f7ae1
WallaceBuena <wallace@buenavista.com.br>
2025-10-27 14:52:59 -0300

    feat: Configura Tailwind CSS via PostCSS e corrige erros iniciais

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Garante que todos os arquivos relevantes sejam escaneados
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
